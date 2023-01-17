6d638de583de2e34893068c3ffcf0bdb4236d7b3
Luiz-de-Lima <luiz12359@gmail.com>
2023-01-17 18:09:07 -0300

    feat:responsividade

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./styles.scss";
 export const Header = () => {
   return (
-    <div>
+    <header>
       <h1 className="title">
         Weather App <span className="span-title">UI</span> Design
       </h1>
-    </div>
+    </header>
   );
 };
